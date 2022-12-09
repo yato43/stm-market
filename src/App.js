fa5e7638c30ad73155fde67686590b3ce4e5d8ed
Yato <akochenokvm@gmail.com>
2022-12-09 19:58:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import {Col, Container, Row, Button,Nav, Navbar, NavDropdown} from "react-bootst
 
 function App() {
   return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav justify-center">
-                  <Nav className="mx-auto">
-                      <Nav.Link href="#Home">Главная</Nav.Link>
-                      <Nav.Link href="#Products">Продукты</Nav.Link>
-                      <Nav.Link href="#pricing">О нас</Nav.Link>
-                      <Nav.Link href="#pricing">Инструменты</Nav.Link>
-                      <Nav.Link href="#pricing">Контакты</Nav.Link>
-                  </Nav>
+      <>
+          <img src={require('./images/logo.png')} alt="" width="50" height="50"/>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+              <Container>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav justify-center">
+                      <Nav className="mx-auto">
+                          <Nav.Link href="#Home">Главная</Nav.Link>
+                          <Nav.Link href="#Products">Продукты</Nav.Link>
+                          <Nav.Link href="#pricing">О нас</Nav.Link>
+                          <Nav.Link href="#pricing">Инструменты</Nav.Link>
+                          <Nav.Link href="#pricing">Контакты</Nav.Link>
+                      </Nav>
 
-              </Navbar.Collapse>
-          </Container>
-      </Navbar>
+                  </Navbar.Collapse>
+              </Container>
+          </Navbar></>
 
   );
 }
