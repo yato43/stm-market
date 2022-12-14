b1cdf1c2a83cb0d6728a34b78ce45a7a5a4a7347
Yato <akochenokvm@gmail.com>
2022-12-14 18:38:03 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import {Button} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <>
+            <Navbar className="justify-content-center nav-bg" expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="md-auto"/>
+                    <Navbar.Collapse id="basic-navbar-nav" >
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#link2">Link</Nav.Link>
+                            <Nav.Link href="#link3">Link</Nav.Link>
+                            <Nav.Link href="#link4">Link</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default App;
