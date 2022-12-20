8fd20d269b163d04e704983932ffb514f0f88df4
Yato <akochenokvm@gmail.com>
2022-12-20 15:46:17 +0300

    new navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
-
+import Navbar from "./components/Navbar"
 function App() {
     return (
-        <>
-            <Navbar className="justify-content-center nav-bg" expand="lg">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="md-auto"/>
-                    <Navbar.Collapse id="basic-navbar-nav" >
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <Nav.Link href="#link2">Link</Nav.Link>
-                            <Nav.Link href="#link3">Link</Nav.Link>
-                            <Nav.Link href="#link4">Link</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <div>
+            <Navbar />
+            <div className="container">
+                <article>
+                    <h1>What is Lorem Ipsum? </h1>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+                </article>
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import "../styles/navbar.scss"
+import {useState} from "react";
+
+export default function Navbar() {
+    const [isHamActive, setIsHamActive] = useState(false)
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+    return (
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+                MacroSoft
+            </a>
+            <button className="hamburger"  onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+            }}>
+                <i
+                    style={{ background: "transparent", border: "none" }}
+                    id="nav-toggle"
+                    className={isHamActive ? "active" : null}
+                    onClick={() => setIsHamActive(!isHamActive)}
+                >
+                    <span />
+                    <span />
+                </i>
+            </button>
+            <div
+                className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
